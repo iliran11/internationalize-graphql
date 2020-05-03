8bb0b612400fdf1113da6f42d2b091616a00c675
Liran Cohen <iliran11@gmail.com>
2020-05-03 19:30:14 +0300

    we can read lang cookie in context

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -17,9 +19,20 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Added context. here you should read the lang cookies.
+  // if you are sending request from graphql playground, please follow those instructions:
+  // https://github.com/prisma-labs/graphql-playground/issues/748#issuecomment-412524510
+  context: ({ req }) => {
+    // now we have the user requeted language
+    const lang = req.cookies.lang;
+  },
+});
 
 const app = express();
+app.use(cookieParser());
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
